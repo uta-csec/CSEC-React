3ec4b2dcefe893942ee26b810bf26b168cdb1d57
Javier Cardoso <Cardoso.Javier@outlook.com>
2023-08-15 00:07:50 -0500

    test

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -15,23 +15,25 @@ export function Sponsors() {
                 Our <span className="beloved">beloved</span> sponsors
               </h1>
               <p>
-                A big thanks to all our{" "}
+                A big thanks to all our
                 <span className="beloved">sponsors</span>!
               </p>
+              {/* 
               <div className="sponsors-cards">
-                {/* UTA */}
+                
                 <SponsorCard
                   frontImage={UTA}
                   backText="UTA text description"
                   link="https://www.uta.edu/academics/schools-colleges/engineering"
                 />
-                {/* ISACA */}
+                
                 <SponsorCard
                   frontImage={ISACA}
                   backText="ISACA text description"
                   link="https://www.isaca.org"
                 />
               </div>
+              */}
             </div>
           </Col>
         </Row>

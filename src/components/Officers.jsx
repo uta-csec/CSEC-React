769a1fa2cc84dbe7a13f717d8b3e181b6d610c30
Javier Cardoso <Cardoso.Javier@outlook.com>
2023-11-07 01:33:46 -0600

    Fixed officers name error

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -64,7 +64,7 @@ export function Officers() {
             alt="description"
           />
           <h4>{officer.title}</h4>
-          <h5>{officer.officerName}i</h5>
+          <h5>{officer.officerName}</h5>
           <a href={officer.linkedIn} target="_blank" rel="noreferrer">
             LinkedIn
           </a>
